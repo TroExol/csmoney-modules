49713e3d6aaedab220ada4f5a8f420fee53c17b6
troexol <zeidxol@gmail.com>
2022-01-30 16:06:06 +0500

    1

--- src/helpers/defaultSetting.js
+++ src/helpers/defaultSetting.js
@@ -100,10 +100,29 @@ const defaultSetting = {
             delay: 0,
         },
     },
-    /**
-     * Установка настроек.
-     * @param {Object} settings - Настройки
-     */
+    /** Установка значений по умолчанию.
+    * @param { object? } settings - Настройки
+    * @param { Array? } settings.appIdList - Массив с id нужных игр.
+    * @param { string? } settings.languageName - Выбор языка для предметов. (ru || en)
+    *
+    * @param { object? } settings.repeatLoad - Настройки обновления данных.
+    *
+    * @param { object? } settings.repeatLoad.myInventory - Настройки обновления инвентаря пользователя.
+    * @param { boolean } settings.repeatLoad.myInventory.status - Включить / Выключить повторное обновление.
+    * @param { number } settings.repeatLoad.myInventory.delay - Выбор языка для предметов. (ru || en)
+    *
+    * @param { object? } settings.repeatLoad.itemNames - Настройки обновления name id.
+    * @param { boolean } settings.repeatLoad.itemNames.status - Включить / Выключить повторное обновление.
+    * @param { number } settings.repeatLoad.itemNames.delay - Время задержки перед повторным обновлением.
+    *
+    * @param { object? } settings.repeatLoad.checkStatus - Настройки обновления статусов предметов.
+    * @param { boolean } settings.repeatLoad.checkStatus.status - Включить / Выключить повторное обновление.
+    * @param { number } settings.repeatLoad.checkStatus.delay - Время задержки перед повторным обновлением.
+    *
+    * @param { object? } settings.repeatLoad.balance - Настройки обновления баланса.
+    * @param { boolean } settings.repeatLoad.balance.status - Включить / Выключить повторное обновление.
+    * @param { number } settings.repeatLoad.balance.delay - Время задержки перед повторным обновлением.
+    */
     set (settings) {
         // Изменяем нужные настройки
         Object.entries(settings).forEach(([settingKey, settingData]) => {
